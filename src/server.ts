71b6ac8d6f5e5ee48f79d2180f226c65d462b2b4
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-11-11 16:04:12 +0530

    add express.js

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from "./app";
 import { Config } from "./config";
 
-console.log("PORT", Config.NODE_ENV);
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            console.log(`listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("error", error);
+        process.exit(1);
+    }
+};
+
+startServer();
