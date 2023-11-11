9a81b62a35067f0d0940ae1b83f51ad2ab943543
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-11-11 14:41:29 +0530

    test husky 4

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 function login(username: string): string {
     const user = {
         name: "ranjeet",
-};
-const name = user["name"];
+    };
+    const name = user.name;
     return username + name;
 }
 login("ranjeet");
