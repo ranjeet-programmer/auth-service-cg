b7301257942b16b7ce5a448f33f70de193f5ac7f
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2023-11-12 12:38:02 +0530

    add test setup

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import { calculateDiscount } from "./src/utlis";
+import request from "supertest";
+import app from "./src/app";
+
+describe("App", () => {
+    it("should calculate the discount", () => {
+        const res = calculateDiscount(1000, 20);
+        expect(res).toBe(200);
+    });
+
+    it("should return 200 status", async () => {
+        const response = await request(app).get("/").send();
+        expect(response.statusCode).toBe(200);
+    });
+});
